c09762da39ca20f8d80a4917fceba187e9f23d79
Samyak-Tiwary <Samyak.tiwary04@gmail.com>
2026-07-05 12:23:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1223344
+let accountEmail = "Samyak12@google.com"
+var accountPassword = "12334"
+accountCity = "Arrah"
+let accountState;
+
+// accountId = 2
+
+accountEmail ="miku12@gmail.com"
+accountPassword = "21313121"
+accountCity = "jaipur"
+
+
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
+
+console.log(accountId);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "Samyak"
+let age = 19
+let isLoggin = false
+let state = null
+
+
+
+
+
+
+
+
+
+
+console.log(typeof undefined);
