536ad90f5690b33747134d7aec272c6a43a07564
Samyak-Tiwary <Samyak.tiwary04@gmail.com>
2026-07-06 10:21:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+let score = "Samyak"
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let isLoggedIn = "Samyak"
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIN);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
