144b57a5062493628999193b4fcce5f813712f65
Samyak-Tiwary <Samyak.tiwary04@gmail.com>
2026-07-20 11:49:09 +0000

    connversion of datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,15 +1,28 @@
+"use strict"; // treat all JS code as a newer version 
+
+//alert( 3+3) // we are using nodeJS, not browser
+
+console.log(3+3)
+console.log("Samyak")
+
 let name = "Samyak"
-let age = 19
-let isLoggin = false
-let state = null
+let age = 22
+let isloggedin = false
 
 
 
 
 
+//number= 2 to power 53
+//bigint
+//Boolean = ture/false
+//null = stand alone value
+//undefined = value not assigned
+// symbols = unique
 
 
 
+//object 
 
-
-console.log(typeof undefined);
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
