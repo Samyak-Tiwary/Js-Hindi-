d897030eb2a3747b6135b5347e0952add19f065a
Samyak-Tiwary <Samyak.tiwary04@gmail.com>
2026-07-21 17:50:30 +0000

    conversion operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -15,5 +15,42 @@ let booleanIsLoggedIN = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+//console.log(stringNumber);
+//console.log( typeof stringNumber);
+
+//*******************Operations************* *//
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+//colsole.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Samyak"
+//console.log(str1 +str2);
+
+//let str3 = str1 + str2 
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log("1"+2+2);
+//console.log(1 +2+"2");
+//console.log(true);
+ 
+     
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
